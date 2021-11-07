ee1071cc8d69a80d89559f3780ce2204558c47b3
Wu Zongze <wuzongze001@gmail.com>
2021-11-07 08:51:29 +0200

    add DCI

--- GetCode.py
+++ GetCode.py
@@ -1,6 +1,6 @@
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1" #(or "1" or "2")
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1" #(or "1" or "2")
 
 
 import os
